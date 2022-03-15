9da36296a07f7ce076e60299d19c39d5b3e1efbc
Jesús Herrera <jesusherrera13@gmail.com>
2022-03-14 21:02:54 -0700

    Curso C#. POO IX. Clases anónimas. Vídeo 35

---
+++ PildorasInformaticas/Video35.cs
@@ -0,0 +1,15 @@
+
+namespace PildorasInformaticas
+{
+    class Video35 {
+        public const string URL = "https://www.youtube.com/watch?v=7JfBhGDmrws";
+        public const string VIDEO_TITULO = "Curso C#. POO IX. Clases anónimas. Vídeo 35";
+
+        public Video35()
+        {
+            var miVariable = new { Titulo = "Interestelar", Anio = 2014 };
+
+            Console.WriteLine(miVariable.Titulo);
+        }
+    }
+}
